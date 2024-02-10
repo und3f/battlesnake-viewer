7593e2987cbbd64b60b0cf4eada25842baa45f09
Serhii Zasenko <sergii@zasenko.name>
2024-02-10 15:01:59 +0200

    Add color parameter

--- src/main/java/name/zasenko/battlesnake/presentation/ConsoleGamePresentation.java
+++ src/main/java/name/zasenko/battlesnake/presentation/ConsoleGamePresentation.java
@@ -4,20 +4,31 @@ import name.zasenko.battlesnake.coding.palette.PaletteCodingImpl;
 import name.zasenko.battlesnake.coding.snake.SnakeCoding;
 import name.zasenko.battlesnake.entities.AsciiMatrix;
 import name.zasenko.battlesnake.entities.MoveRequest;
+import name.zasenko.battlesnake.mapper.asciimatrix.GameToAsciiMatrixMapper;
+import name.zasenko.battlesnake.mapper.asciimatrix.GameToBasicAsciiMatrixMapper;
 import name.zasenko.battlesnake.mapper.asciimatrix.GameToColorfulAsciiMatrixMapper;
 import name.zasenko.battlesnake.mapper.stringifier.AsciiMatrixStringifier;
 import name.zasenko.battlesnake.mapper.asciimatrix.GridDecorator;
 
 public class ConsoleGamePresentation implements GamePresentation {
-    private final SnakeCoding coding;
+    private final GameToAsciiMatrixMapper mapper;
+
+    public ConsoleGamePresentation(SnakeCoding coding, Boolean color) {
+        if (color) {
+            mapper = new GameToColorfulAsciiMatrixMapper(coding, new PaletteCodingImpl());
+        } else {
+            mapper = new GameToBasicAsciiMatrixMapper(coding);
+        }
+    }
 
     public ConsoleGamePresentation(SnakeCoding coding) {
-        this.coding = coding;
+        this(coding, false);
     }
 
     @Override
     public void execute(MoveRequest moveRequest) {
-        AsciiMatrix boardGrid = new GameToColorfulAsciiMatrixMapper(coding, new PaletteCodingImpl()).buildBoardGrid(moveRequest);
+
+        AsciiMatrix boardGrid = mapper.buildBoardGrid(moveRequest);
         boardGrid = GridDecorator.execute(boardGrid);
 
         System.out.println();
